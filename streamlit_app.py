bd3d7fd901acff711f90310dd282ea48c22da20b
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-01-10 12:19:53 -0800

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 and Blueberry Oatmeal')
-strealmit.text('Kale, Spinach, & Rocket Smoothie')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
