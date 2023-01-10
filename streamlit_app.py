8ed84cb779cce519ad511459e81758ea475c1d9c
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-01-10 12:18:14 -0800

    Update streamlit_app.py
    Format Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
+
 streamlit.title('My Parents New Healthy Dinner')
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 strealmit.text('Kale, Spinach, & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
