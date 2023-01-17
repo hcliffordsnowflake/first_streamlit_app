38e3cb1737a8186dbad19ef0a82e5aee21137e75
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-01-17 12:31:56 -0800

    Update streamlit_app.py
    Format the API output into a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+
+# Format the JSON code 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Produce the output in a table
+streamlit.dataframe(fruityvice_normalized)
