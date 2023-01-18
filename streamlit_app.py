0182fab6fbdb1121ea894192db2331effa699f28
Heather Clifford <122399975+hcliffordsnowflake@users.noreply.github.com>
2023-01-17 16:33:11 -0800

    Update streamlit_app.py
    Added another text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+
+# Adding another text box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thank you for adding ', add_my_fruit)
